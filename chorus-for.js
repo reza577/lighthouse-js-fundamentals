822f7c9c7d41966838385848f46a5140dbf9fe08
reza rahnama <rezarhnm@gmail.com>
2019-02-20 02:03:49 +0000

    adds loping exercises

---
+++ chorus-for.js
@@ -0,0 +1,9 @@
+var chorus = "lets dance";
+for (var repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+console.log("until the sun comes down");
+
+
+
+
---
+++ chorus-while.js
@@ -0,0 +1,7 @@
+var chorus = "lets dance";
+var repeat = 0;
+while (repeat < 10) {
+  console.log(chorus);
+  repeat++;
+}
+console.log("untill the sun comes up!");
